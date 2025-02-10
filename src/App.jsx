aa9113fbc460a07a72edc9edb56f5adfda67ec9f
Tetiana Baranova <syniovych.tania@gmail.com>
2025-02-10 21:21:59 +0200

    searchbar initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
+import SearchBar from './components/SearchBar/SearchBar';
+import getImages from './services/api'
 import './App.css'
-import axios from "axios";
+
+// import axios from "axios";
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(false);
+  const [image, setImage] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [totalPage, setTotalPage] = useState(0);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectImage, setSelectImage] = useState(0);
+
+
+
 
   return (
     <>
-      
+      <SearchBar />
     </>
   )
 }
